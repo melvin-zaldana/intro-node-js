859aeee8932cc5ddb8063c95d89c77a1da4dbb06
Melvin Zaldana <melvinzaldana91@gmail.com>
2021-06-29 09:05:09 -0500

    Modulo sencillo con saludos

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    hola: function(){
+        console.log("Hola mundo");
+    },
+    holaPersonalizado: function(nombre){
+        console.log(`Hola ${nombre}`);
+    },
+    holaAlCursoNPM: function(){
+        console.log("Hola al curso NPM!!!");
+    }
+}
